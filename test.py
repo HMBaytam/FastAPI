3b1240e78569daa2fc05102392a41aff4c20a0b9
Hassan Elbaytam <hassan.elbaytam@gmail.com>
2021-12-02 20:26:15 -0500

    testing github copilot

--- test.py
+++ test.py
@@ -1,1 +1,33 @@
 # print hello world
+print("Hello World!")
+
+# create a variable and assign it a value of 549
+my_variable = 549
+
+# function that takes 2 variables and returns the product of both variables
+def multiply(x, y):
+    return x * y
+
+# function that takes 2 variables and returns the first variable raised to the power of the second variable
+def power(x, y):
+    return x ** y
+
+# function that takes 2 variables and returns the second variable raised to the power of the first variable
+def power2(x, y):
+    return y ** x
+
+# functions that changes the variable type from int to string
+def int_to_string(x):
+    return str(x)
+
+# function that chages the variable type from string to int
+def string_to_int(x):
+    return int(x)
+
+# function that analyzes the length of a string
+def string_length(x):
+    return len(x)  
+
+# function that returns the first letter of a string
+def first_letter(x):
+    return x[0]
